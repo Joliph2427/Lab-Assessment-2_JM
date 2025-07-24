76376b2f30157bb0dee0422ae85c200c27737b86
Joliph2427 <joliph01macwan@gmail.com>
2025-07-24 18:36:21 -0400

    commit
    commit

--- scripts/app.js
+++ scripts/app.js
@@ -5,26 +5,32 @@ const app = Vue.createApp({
         return {
                 studentName: 'Joliphkumar',
                 gasLevelValue: 55,
-                gasLevels: [{ id: 'danger-low', color: 'danger', name: 'Danger - Low Gas Levels!',
-min: 0, max: 10 },{ id: 'warn-low', color: 'warning', name: 'Warning - Low Gas Levels',
-min: 11, max: 31 },{ id: 'safe', color: 'safe', name: 'Safe Gas Levels', min: 32, max: 70 },{ id: 'warn-high', color: 'warning', name: 'Warning - High Gas Levels',
-min: 71, max: 91 },{ id: 'danger-high', color: 'danger', name: 'Danger - High Gas Levels!',
-min: 92, max: 100 }]
+                gasLevels: [
+{ id: 'danger-low', color: 'danger', name: 'Danger - Low Gas Levels!',
+min: 0, max: 10 },
+ { id: 'warn-low', color: 'warning', name: 'Warning - Low Gas Levels',
+min: 11, max: 31 },
+ { id: 'safe', color: 'safe', name: 'Safe Gas Levels', min: 32, max: 70 },
+ { id: 'warn-high', color: 'warning', name: 'Warning - High Gas Levels',
+min: 71, max: 91 },
+ { id: 'danger-high', color: 'danger', name: 'Danger - High Gas Levels!',
+min: 92, max: 100 }
+ ]
 
         };
     },
     // Define the methods for the app
     methods: {
         activeGasLevel(gasLevel) {
-            // The logic goes here for if the gas level is within the level parameters return boolean
+            // // The logic goes here for if the gas level is within the level parameters return boolean
             if(this.gasLevelValue>=gasLevel.min && this.gasLevelValue<=gasLevel.max)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+             {
+                 return true;
+             }
+             else
+                {
+                    return false;
+                }
         }
     }
 });
