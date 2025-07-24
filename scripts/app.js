cad8fe03436075423781f1207793764fd68706cd
Joliph2427 <joliph01macwan@gmail.com>
2025-07-24 18:22:46 -0400

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -9,7 +9,7 @@ const app = Vue.createApp({
 min: 0, max: 10 },{ id: 'warn-low', color: 'warning', name: 'Warning - Low Gas Levels',
 min: 11, max: 31 },{ id: 'safe', color: 'safe', name: 'Safe Gas Levels', min: 32, max: 70 },{ id: 'warn-high', color: 'warning', name: 'Warning - High Gas Levels',
 min: 71, max: 91 },{ id: 'danger-high', color: 'danger', name: 'Danger - High Gas Levels!',
-min: 92, max: 100 }],
+min: 92, max: 100 }]
 
         };
     },
